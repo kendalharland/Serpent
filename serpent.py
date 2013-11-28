d8fbdde9ffc5125acbd9151fd3dfff000e827c3b
Kendal Harland <kharland2011@gmail.com>
2013-11-28 05:43:43 -0600

    Update serpent.py

--- serpent.py
+++ serpent.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python
 
-"""python
+"""Serpent
 
 Badassify your python script by turning your code
 into an actual python wrapped around a sword. I'm 
 not shitting you.
+
+(More documentation to come later) 
+
+The MIT License (MIT)
+
+Copyright (c) 2013 Kendal Harland
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 """
 
 import compiler
